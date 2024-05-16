68f7cd06f229067d0508654953e38f3fe06f8a5e
090105 <lnj090105@gmail.com>
2024-05-16 10:11:04 +0200

    Edits button press function

--- Godot/scripts/column_adder.cs
+++ Godot/scripts/column_adder.cs
@@ -22,7 +22,6 @@ public partial class column_adder : Button
 	}
 	 void _on_timer_timeout()
 			{
-			GD.Print("Button pressed");
 			row.AddChild(scene.Instantiate());
 		GD.PrintRich($"added [b]{scene.Instantiate().Name}[/b] to [b]{row.Name}[/b]");
 			}
@@ -32,16 +31,13 @@ public partial class column_adder : Button
 		GD.PrintRich($"added [b]{scene.Instantiate().Name}[/b] to [b]{row.Name}[/b]");
 	}
 
-	private void _on_area_2d_area_entered(Area2D area)
+	private void _on_area_2d_body_entered(Area2D area)
  {
-	// Replace with function body.
-	GD.Print("Mouse entered the button area...");
 	delayTimer.Start();
 }
 
-private void _on_area_2d_area_exited(Area2D area)
+private void _on_area_2d_body_exited(Area2D area)
 {
-	GD.Print("Mouse exit on button area.."); // Replace with function body.
 	delayTimer.Stop();
 }
 }
